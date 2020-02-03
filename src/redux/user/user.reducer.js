c79f249326a6a69c83e90f9b358acdf0a6d29a78
mayowa_ola <mayowaoladunjoye@gmail.com>
2020-02-04 00:51:22 +0100

    Redux added to application along with user reducer and added it to users and header component

---
+++ src/redux/user/user.reducer.js
@@ -0,0 +1,19 @@
+//Instantiating the intial state of the user reducer
+
+const INITIAL_STATE = {
+    currentUser: null
+}
+
+const userReducer = (currentState = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case 'SET_CURRENT_USER' :
+            return {
+                ...currentState,
+                currentUser: action.payload
+            };
+        default:
+        return currentState;
+    }
+}
+
+export default userReducer;
