a250ff8bae21c6c2dbb1d708502f3dd146fd75e4
mayowa_ola <mayowaoladunjoye@gmail.com>
2020-02-06 02:04:57 +0100

    updated cart reducer to group cart added item based on quantity

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import CustomButton from '../custom-button/custom-button.component';
+import { connect } from 'react-redux';
+import {addItem} from '../../redux/cart/cart.actions';
+
 
 import './collection-item.styles.scss';
 
-const CollectionItem = ({imageUrl,name,price}) => {
+const CollectionItem = ({item, addItem}) => {
+    const { name, price, imageUrl} = item;
     return(
         <div className="collection-item">
             <div className="image"
@@ -12,10 +17,17 @@ const CollectionItem = ({imageUrl,name,price}) => {
                 <span className="name">{name}</span>
                 <span className="price">{price}</span>
             </div>
+            <CustomButton onClick={() => addItem(item)} inverted> Add To cart</CustomButton>
         </div>
     )
 }
 
-export default CollectionItem;
+
+
+const mapDispatchToProps =  dispatch => ({
+    addItem: item => dispatch(addItem(item))
+ })
+
+export default connect(null, mapDispatchToProps) (CollectionItem);
 
  
